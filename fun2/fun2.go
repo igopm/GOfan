d8dc4ca64fab40b9777e3f677b1f33aa9db1f49c
igopm <igor.mashch@gimail.com>
2017-12-12 17:22:53 +0200

    fun2

---
+++ fun2/fun2.go
@@ -0,0 +1,18 @@
+// fun1 project fun2.go
+package main
+
+import "fmt"
+
+func main() {
+	// Strings, which can be added together with `+`.
+	fmt.Println("go" + "lang")
+
+	// Integers and floats.
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// Booleans, with boolean operators as you'd expect.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
